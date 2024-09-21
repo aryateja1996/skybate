28b7376ee09b8e39e7a0c6f625323b6ed4bebb35
aryateja1996 <sreekarkondaveeti76@gmail.com>
2024-09-21 11:17:43 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ServiceComponent } from './pages/servicees/service.component';
+import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { SecurityComponent } from './pages/security/security.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: "full" },
+    { path: 'home', component: HomeComponent },
+    { path: 'contact', component: ContactusComponent },
+    { path: 'about-us', component: AboutusComponent },
+    { path: "our-services", component: ServiceComponent },
+    { path: "privacy-policy", component: PrivacyPolicyComponent },
+    { path: "terms-conditions", component: TermsAndConditionsComponent },
+    {path: "security-policy", component: SecurityComponent}
+];
