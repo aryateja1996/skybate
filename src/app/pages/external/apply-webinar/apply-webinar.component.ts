b7321898983d15321ef6802b8f1ba7a112875fc9
sreekar1996 <sreekarkondaveeti76@gmail.com>
2024-09-27 03:03:19 +0530

    workflow first commit

--- src/app/pages/external/apply-webinar/apply-webinar.component.ts
+++ src/app/pages/external/apply-webinar/apply-webinar.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../../services/api.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-apply-webinar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './apply-webinar.component.html',
   styleUrl: './apply-webinar.component.css'
 })
 export class ApplyWebinarComponent {
+  constructor(private apiService: ApiService){
 
+  }
+  participant = {
+    name: '',
+    email: '',
+    phone: '',
+    experience: 'beginner',
+    session: 'morning',
+    interest: '',
+    requirements: '',
+    agreeToTerms: false
+  };
+
+  onSubmit() {
+    if (this.participant.agreeToTerms) {
+      // Logic for form submission can go here (e.g., HTTP request to backend)
+      console.log('Form Submitted:', this.participant);
+      alert('Registration successful!');
+    } else {
+      alert('Please agree to the terms and conditions.');
+    }
+  }
 }
